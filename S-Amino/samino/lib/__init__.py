3e17c5a3a5945c42fc8f80e45d4d7f938cdbe0ee
SirLez <76112117+SirLez@users.noreply.github.com>
2021-04-03 11:41:37 +0300

    Delete S-Amino directory

--- S-Amino/samino/lib/__init__.py
+++
@@ -1,3 +0,0 @@
-import objects
-import pack
-from samino import GlobalClient, LocalClient
--- S-Amino/samino/lib/apis.py
+++
@@ -1,2 +0,0 @@
-# requests.post(url=f'{self.api+comId+self.chat+chatId}/member/{uid}', headers=self.headers)
-# requests.delete(url=f'{self.api+comId+self.chat+chatId}/member/{uid}', headers=self.headers)
