a940f401abcca8d7d1a73744aedbfb8951ebe6ea
SirLez <76112117+SirLez@users.noreply.github.com>
2021-04-03 13:00:52 +0300

    Update __init__.py

--- S-Amino/amino/__init__.py
+++ S-Amino/amino/__init__.py
@@ -6,7 +6,7 @@ from json import loads
 
 
 try:
-    __version__ = '1.1.1'
+    __version__ = '1.1.3'
     __newest__ = loads(get("https://pypi.python.org/pypi/SAmino/json").text)["info"]["version"]
     if __version__ != __newest__:
         print(f"SAmino New Version!: {__newest__} (Your Using {__version__})")
